ecf3a04f05bdac30ab5050e3f7c29a9aa2a7b9bd
kanikajain2 <kanikaj0210@gmail.com>
2025-08-15 13:23:34 +0530

    Update UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:health_broo/extra.dart';
 import 'package:health_broo/firebase_options.dart';
 import 'package:health_broo/frontpage.dart';
 
